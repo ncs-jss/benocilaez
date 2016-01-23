7528184f334772e6ad663c4c7d021b588337f8d9
Abhay <abhayrawat2695@gmail.com>
2016-01-23 13:32:39 +0530

    cleaning project

--- app/Http/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-Route::get('/',['as'=>'login','uses'=>'pagesController@login']);
-Route::get('/register',['as'=>'registration','uses'=>'pagesController@registration']);
-
-Route::post('/verification',['as'=>'verification','uses'=>'UserController@ulogin']);
-Route::post('/signup',['as'=>'signup','uses'=>'UserController@usersignup']);
-?>
--- resources/views/event.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-</head>
-<body>
-<h1>"Event name"</h1>
-<p>"Description"</p>
-</body>
-</html>
