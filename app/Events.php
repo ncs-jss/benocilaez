b4c6a288739a67fc8d3a20ad608f48ad69900ba6
Abhay <abhayrawat2695@gmail.com>
2016-01-24 18:32:44 +0530

    routes added and readme edited

--- app/Events.php
+++ app/Events.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model as Model;
 
 class Events extends Model{
 	protected $fillable = ['society_email', 'event_id'];
-	
+	protected $hidden = [
+        'created_at', 'updated_at','remember_token',
+    ];
 }
