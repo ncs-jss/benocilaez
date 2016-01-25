1f430619c84e1e28fe8bdb17c6b5db3688ba9475
sahilkmaurya <sahil.kr.maurya@gmail.com>
2016-01-25 16:38:52 +0530

    bootstrap CDN removed

--- resources/views/add_event.blade.php
+++ resources/views/add_event.blade.php
@@ -2,13 +2,6 @@
 <html>
 <head>
 <title>Add Events</title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 <script src="//cdn.ckeditor.com/4.5.6/standard/ckeditor.js"></script>
 <style>
 .desc{
@@ -31,20 +24,22 @@
 				<label for="event_name" class="col-md-4 control-label">Event Name</label>
 					<div class="col-md-5">
 						@if($action == 'Add Event')
-						<input type="text" name="event_name" class="form-control" placeholder="Event Name">
+						<input type="text" name="event_name" class="form-control">
 						@else
 						<p>{{ $event_name }}<p>
 						@endif
 					</div>
 				</div><br>
 				<div class="form-group">
-					<div class="col-md-5">
+					<div class="col-md-3"></div>
+					<div class="col-md-7">
 						<textarea class="sdesc" rows="5" name="short_description" class="form-control" placeholder="A Short Description of Your Event..."></textarea>
 					</div>
 				</div><br>
 				<div class="form-group">
-					<div class="col-md-5">
-						<textarea class="desc" id="editor1" name="editor1" placeholder="Enter Your Event's Description Here.."></textarea>
+					<div class="col-md-3"></div>
+					<div class="col-md-7">
+						<textarea class="desc" id="editor1" name="editor1">Your Event's Description Here...</textarea>
 						<script type="text/javascript">
 							CKEDITOR.replace( 'editor1' );
 						</script>
@@ -61,9 +56,23 @@
 					</div>
 				</div><br>
 				<div class="form-group">
-					<label for="winners" class="col-md-4 control-label">Number of Winners:</label>
-					<div class="col-md-5">
-						<input type="text" name="winners" class="form-control" placeholder="Number of Winners">
+					<label for="contact" class="col-md-4 control-label">Contacts</label>
+					<div class="col-md-2">
+						<input type="text" name="contact_name" class="form-control" placeholder="Name">
+					</div>
+					<div class="col-md-1"></div>
+					<div class="col-md-2">
+						<input type="text" name="conatct_number" class="form-control" placeholder="Number">
+					</div>
+				</div><br>
+				<div class="form-group">
+					<div class="col-md-4"></div>
+					<div class="col-md-2">
+						<input type="text" name="contact_name" class="form-control" placeholder="Name">
+					</div>
+					<div class="col-md-1"></div>
+					<div class="col-md-2">
+						<input type="text" name="conatct_number" class="form-control" placeholder="Number">
 					</div>
 				</div><br>
 				<div> {{ $err }}</div>
