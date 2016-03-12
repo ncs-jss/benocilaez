77d37efba9060603eff82dd9b714f797676c1460
Abhay <abhayrawat2695@gmail.com>
2016-03-12 19:31:44 +0530

    ..

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -13,21 +13,7 @@
 
     <ul class="nav navbar-top-links navbar-right">
         <!-- /.dropdown -->
-        <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
-            </a>
-            <ul class="dropdown-menu dropdown-messages">
-                <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-                </li>
-                <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-                </li>
-                <li class="divider"></li>
-                <li><a href="login.html"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-                </li>
-            </ul>
-            <!-- /.dropdown-user -->
-        </li>
+        <a href="logout"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
         <!-- /.dropdown -->
     </ul>
     <!-- /.navbar-top-links -->
