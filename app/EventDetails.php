72d7392db89df41c2d9785cf1bab1528ce84d679
Nelabh Kotiya <nelabhkotiya@gmail.com>
2016-03-31 19:07:00 +0530

    Data Populated

--- app/EventDetails.php
+++ app/EventDetails.php
@@ -4,8 +4,8 @@ namespace App;
 use Illuminate\Database\Eloquent\Model as Model;
 class EventDetails extends Model{
 	protected $fillable = ['event_name','event_description', 
-	'event_id', 'prize_money', 'timing', 'approved', 'attachment', 'long_des', 'rules'];
+	'event_id', 'prize_money', 'timing', 'approved', 'attachment', 'long_des', 'rules','first_place','second_place','grp'];
 	protected $hidden = [
 	'created_at', 'updated_at','remember_token',
 	];
-}
+}	
