69fb2f26115ddeda4237c14a3c6917f718a857c4
Anant Jain <anantjain60@gmail.com>
2018-10-26 17:24:41 +0530

    Installed Passport

--- server/app/Society.php
+++ server/app/Society.php
@@ -2,11 +2,13 @@
 
 namespace App;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Society extends Authenticatable
 {
+	use HasApiTokens, Notifiable;
     protected $table = 'society';
     public $timestamps = false;
 }
