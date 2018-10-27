f26580939de1f877a84747228cab347b82667240
Anant Jain <anantjain60@gmail.com>
2018-10-27 17:29:57 +0530

    Elequant Relationships

--- server/app/Winner.php
+++ server/app/Winner.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Winner extends Model
 {
     public $timestamps = false;
+
+    /**
+     * Get the event whoes winner is this.
+     */
+    public function event()
+    {
+        return $this->belongsTo('App\Event', 'event_id');
+    }
 }
