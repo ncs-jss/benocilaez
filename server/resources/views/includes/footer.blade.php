f95d7be643899de8c324197eaf1b6b15ed4c88c1
Rohit599 <rjakhmola5@gmail.com>
2018-11-24 18:41:47 +0530

    Add includes

---
+++ server/resources/views/includes/footer.blade.php
@@ -0,0 +1,38 @@
+        <footer class="footer text-center">
+            All Rights Reserved by Matrix-admin. Designed and Developed by <a href="https://wrappixel.com">WrapPixel</a>.
+        </footer>
+        <!-- ============================================================== -->
+        <!-- End footer -->
+        <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- End Page wrapper  -->
+    <!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- End Wrapper -->
+<!-- ============================================================== -->
+<!-- ============================================================== -->
+<!-- All Jquery -->
+<!-- ============================================================== -->
+<script src="../../assets/libs/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap tether Core JavaScript -->
+<script src="../../assets/libs/popper.js/dist/umd/popper.min.js"></script>
+<script src="../../assets/libs/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="../../assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js"></script>
+<script src="../../assets/extra-libs/sparkline/sparkline.js"></script>
+<!--Wave Effects -->
+<script src="../../dist/js/waves.js"></script>
+<!--Menu sidebar -->
+<script src="../../dist/js/sidebarmenu.js"></script>
+<!--Custom JavaScript -->
+<script src="../../dist/js/custom.min.js"></script>
+<!--This page JavaScript -->
+<!-- <script src="../../dist/js/pages/dashboards/dashboard1.js"></script> -->
+<!-- Charts js Files -->
+<script src="../../assets/libs/flot/excanvas.js"></script>
+<script src="../../assets/libs/flot/jquery.flot.js"></script>
+<script src="../../assets/libs/flot/jquery.flot.pie.js"></script>
+<script src="../../assets/libs/flot/jquery.flot.time.js"></script>
+<script src="../../assets/libs/flot/jquery.flot.stack.js"></script>
+<script src="../../assets/libs/flot/jquery.flot.crosshair.js"></script>
+<script src="../../assets/libs/flot.tooltip/js/jquery.flot.tooltip.min.js"></script>
+<script src="../../dist/js/pages/chart/chart-page-init.js"></script>
